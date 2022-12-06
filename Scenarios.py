0421e22373ea62d4b9573dd94a0f611cc435a52b
HugoChass <62014029+HugoChass@users.noreply.github.com>
2022-12-06 13:52:47 +0100

    added obstacle creation class

--- Scenarios.py
+++ Scenarios.py
@@ -1,18 +1,36 @@
 import shapely.geometry
 import matplotlib.pyplot as plt
 
-coords = [(0, 0), (0, 10), (10, 10), (10,0),(0,0)]
-Boundary = shapely.geometry.LineString(coords)
-polygon1 = shapely.geometry.Polygon([(5,6),(7,1), (8,2),(7,5)])
-polygon2 = shapely.geometry.Polygon([(1,4),(2,2), (3,1),(1,4)])
-polys = shapely.geometry.GeometryCollection([polygon1, polygon2])
+class create_obstacles:
+    def __init__(self):
+        self.polygons = []
+        pass
+    
+    def square_boundary(self, x,y):
+        coords = [(0, 0), (0, y), (x, y), (x,0), (0,0)]
+        self.Boundary = shapely.geometry.LineString(coords)
+        return self.Boundary
 
-for geo in polys.geoms:
-    if geo.intersects(Boundary):
-        print("interstion")
+    def special_boundary(self, coords):# ie: coords = [(5,6),(7,1), (8,2),(7,5), (5,6)] first and last point must be same
+        self.Boundary = shapely.geometry.LineString(coords)
+        return self.Boundary
 
-plt.plot(*Boundary.xy)
-for geo in polys.geoms:
-    x,y = geo.exterior.xy
-    plt.plot(x,y)
-plt.show()
+    def polygon(self, coords): # ie: coords = [(5,6),(7,1), (8,2),(7,5)]
+        Polygon = shapely.geometry.Polygon(coords)
+        self.polygons.append(Polygon)
+        return Polygon
+
+    def rectangle(self, width, height, left_corner):
+        x, y = left_corner
+        Polygon = shapely.geometry.Polygon([x, y],[x, y + height], [x + height, y + height], [x + height, y], [x, y])
+        self.polygons.append(Polygon)
+        return Polygon
+
+    def Boundary_collision_check(self, geom):
+        return geom.intersects(self.Boundary)
+
+    def plotting(self):
+        plt.plot(*self.Boundary.xy)
+        for geom in self.polygons:
+            plt.plot(*geom.exterior.xy)
+        plt.show()
