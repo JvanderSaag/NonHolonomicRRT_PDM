e9d9597df04bbf14f3c4483d621aca97deffd565
Jelmer <jelmervandersaag@me.com>
2023-01-09 15:47:13 +0100

    RRT* made more robust, and the option to use RRT (star=False) added as a parameter

--- RRT.py
+++ RRT.py
@@ -17,12 +17,12 @@ class TreeNode: # Tree Node class that RRT uses
         self.children = [] # Children, only used to draw the entire RRT tree
 
 
-def RRT(N_iter, scenario, step_size=float('inf'), dist_tolerance=0.2, goal_prob=0.05): # RRT using TreeNodes
+def RRT(N_iter, scenario, step_size=float('inf'), dist_tolerance=0.2, goal_prob=0.05, star=True): # RRT using TreeNodes
+    # Initialise start and goal node
     start_Node, goal_Node = TreeNode(scenario.start), TreeNode(scenario.goal)
 
     for n in tqdm(range(N_iter)): # Max N_iter iterations
-        # Have a chance of picking the goal node
-        if np.random.random_sample() < goal_prob:
+        if np.random.random_sample() < goal_prob: # Have a chance of picking the goal node as the sampled node
             sampled_Node = goal_Node
         else:  # Otherwise, randomly sample a point in the environment
             sampled_Node = TreeNode(Point(rand_coords(scenario.width, scenario.height)))
@@ -32,44 +32,63 @@ def RRT(N_iter, scenario, step_size=float('inf'), dist_tolerance=0.2, goal_prob=
             continue # Continue to next iteration
 
         # Find the closest node to sampled point
-        parent_Node, path_to_parent, _ = find_best_Node(scenario, start_Node, sampled_Node, step_size)
+        parent_Node, path_to_parent, _ = find_closest_Node(scenario, start_Node, sampled_Node)
 
-        # Find nearby points x
-        if parent_Node is not None:  # If a nearest node is found     
+        if parent_Node is not None and not parent_Node.point.equals(sampled_Node.point):  # If a nearest node is found     
             if path_to_parent.length > step_size: # In the case that this parent node is not within the step size
                 # Find a new point on the connecting line that is within the radius
                 new_Node_inradius = TreeNode(path_to_parent.interpolate(step_size))
-                new_connect_line = LineString([parent_Node.point, new_Node_inradius.point])
 
                 # Update the sampled Node to this new point
                 sampled_Node = new_Node_inradius
-                path_to_parent = new_connect_line
-
-            # Update the sampled_Node's parent, path and cost
-            sampled_Node.parent = parent_Node 
-            sampled_Node.path_to_parent = path_to_parent 
+                        
+            # Update the sampled_Node cost
             sampled_Node.cost = parent_Node.cost + path_to_parent.length 
-
-            # Add children to the parent
-            parent_Node.children.append(sampled_Node)
             
+            if star: # If RRT* is used, the tree will be redrawn
+                # Within a certain radius, find nearby points
+                radius = 5
+                Nodes_near_sample = find_nearby_nodes(start_Node, sampled_Node, radius, nearby_Nodes=[])
+
+                if Nodes_near_sample: # If there are nearby nodes
+                    min_cost = sampled_Node.cost # First set the upper bound of cost (current cost of sampled Node)
+                    for node in Nodes_near_sample: # For each nearby node
+                        cost_via_node = node.cost + sampled_Node.point.distance(node.point) # Cost estimator using distance
+                        if cost_via_node < min_cost: # If this cost is less, update the parent Node
+                            parent_Node, min_cost = node, cost_via_node
+                            sampled_Node.cost = min_cost
+
+            # Create the path to parent and check if it is collision-free
+            path_to_parent = create_connector(parent_Node.point, sampled_Node.point) # NEW CONNETOR FUNCTION
+            if not scenario.collision_free(path_to_parent):
+                continue # If it collides, continue to next iteration
+
+            # Update parent node with children, and sampled Node with parent
+            parent_Node.children.append(sampled_Node)
+            sampled_Node.parent = parent_Node 
+            sampled_Node.path_to_parent = create_connector(parent_Node.point, sampled_Node.point) 
+
+            # Early stop if normal RRT is used, as once the goal is reached the path won't change
+            if not star and sampled_Node.point.distance(goal_Node.point) < dist_tolerance:
+                final_path = extract_path(sampled_Node)
+                total_tree = extract_all_edges(start_Node)
+                
+                # Update path and tree attributes of class
+                scenario.set_path(final_path)
+                scenario.set_totaltree(total_tree)
+
+                print(f"\nRRT finished within {n} iterations")
+                return
+
+    # If it does not converge, there will be no Nodes near the goal
     Nodes_near_goal = find_nearby_nodes(start_Node, goal_Node, dist_tolerance)
-    
-    shortest_path, min_cost = None, float('inf')
-    
-    # If it does not converge, this code will be reached
     if not Nodes_near_goal:
         raise Exception("\nRRT could not find a suitable path within the given number of iterations. Please try again.\nIf it consistently fails to complete, increase the number of iterations.")
-
-    # Else a path has been found, find the shortest one if there are several
-    for node in Nodes_near_goal:
-        path = extract_path(node, path=[]) # Extract path to start node
-        cost = 0
-        for connector in path:
-            cost += connector.length # Compute total cost of path
-        if cost < min_cost: 
-            shortest_path, min_cost = path, cost # Select only the shortest
     
+    # Else RRT* has found a path, find the shortest one if there are several
+    Node_min_cost = min(Nodes_near_goal, key=lambda x: x.cost)
+    shortest_path = extract_path(Node_min_cost)
+
     # Finally set final path and 'total' tree containing all edges
     final_path = shortest_path 
     total_tree = extract_all_edges(start_Node)
@@ -77,8 +96,9 @@ def RRT(N_iter, scenario, step_size=float('inf'), dist_tolerance=0.2, goal_prob=
     # Update class attributes
     scenario.set_path(final_path)
     scenario.set_totaltree(total_tree)
-
-    print(f"\nRRT finished within {n+1} iterations")
+    
+    # RRT* is done
+    print(f"\nRRT* finished within {n+1} iterations")
     return
 
 
@@ -88,32 +108,31 @@ def rand_coords(width, height): # Generate random coordinates within bounds of e
     return Point(x, y)
 
 
-def find_best_Node(scenario, start_Node, new_Node, min_cost=float('inf')): # Find nearby Node in Tree with lowest cost
-    best_Node, best_path = None, None # Initalise nearest Node and shortest path as None
-    radius = 5 # Radius around which points are considered
+def find_closest_Node(scenario, start_Node, new_Node, min_length=float('inf')): # Find closest Node in Tree
+    nearest_Node, shortest_path = None, None # Initalise nearest Node and shortest path as None
 
-     # Recursively check all children
+    # Recursively check all children
     if start_Node.children: # If Node has children
         for child in start_Node.children: # Iterate over the children nodes
-            temp_Node, temp_path, temp_cost = find_best_Node(scenario, child, new_Node, min_cost=min_cost)
-            if temp_Node and temp_path.length < radius and temp_cost < min_cost:
-                best_Node, best_path, min_cost = temp_Node, temp_path, temp_cost
-   
+            temp_Node, temp_path, temp_length = find_closest_Node(scenario, child, new_Node, min_length=min_length)
+            if temp_length < min_length:
+                nearest_Node, shortest_path, min_length = temp_Node, temp_path, temp_length
+    
+    # Either there are no children, or the recursive search is done (following code will be reached) #
     # CONNECTOR FUNCTION #
-    connect_line = LineString([start_Node.point, new_Node.point]) # Create a line that connects to the new Node
+    connect_line = create_connector(start_Node.point, new_Node.point) # Create a line that connects to the new Node
 
     # COLLISION CHECK #
-    if scenario.collision_free(connect_line): # If the line does not collide with the environment   
-        # Find length of connecting line and cost of this node
-        length = connect_line.length
-        cost = start_Node.cost
+    if scenario.collision_free(connect_line): # If the line does not collide with the environment
+        length = connect_line.length # Find length of connecting line
 
-        # If this cost is less than the saved min cost, and within a radius of the new point
-        if length < radius and cost < min_cost:
-            best_Node = start_Node # set nearest node
-            best_path = connect_line # define the shortest path
-            min_cost = best_Node.cost # then update minimum cost
-    return best_Node, best_path, min_cost
+        # If this length is less than the saved min
+        if length < min_length:
+            nearest_Node = start_Node # set nearest node
+            shortest_path = connect_line # define the shortest path
+            min_length = length # then update minimum length
+       
+    return nearest_Node, shortest_path, min_length
 
 
 def extract_all_edges(start_Node, total_tree=[]): # Extract all edges from tree
@@ -127,7 +146,7 @@ def extract_all_edges(start_Node, total_tree=[]): # Extract all edges from tree
 def find_nearby_nodes(start_Node, goal_Node, tol, nearby_Nodes=[]): # Find Nodes that are near the goal if several paths exist
     if start_Node.children: # Iterate over all the children of the start_Node
         for child in start_Node.children: # For each child
-            if child.point.distance(goal_Node.point) < tol: # If the child is within the tolerated distance of the goal
+            if child.point.distance(goal_Node.point) < tol: # If the child is within the tolerated distance of goal node
                 nearby_Nodes.append(child) # Add the node to the nearby_Nodes list
             find_nearby_nodes(child, goal_Node, tol, nearby_Nodes) # Recursively repeat over all its children
     return nearby_Nodes
@@ -138,3 +157,8 @@ def extract_path(final_Node, path=[]): # Extract only final path from tree
         path.append(final_Node.path_to_parent) # Append the path_to_parent to final path
         extract_path(final_Node.parent, path) # Recursively repeat one layer up
     return path
+
+
+def create_connector(Point1, Point2):
+    connector = LineString([Point1, Point2])
+    return connector
