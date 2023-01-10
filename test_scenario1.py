1df0487568cd50a7403644b63f34cfb6a0b777e6
HugoChass <62014029+HugoChass@users.noreply.github.com>
2023-01-10 14:26:27 +0100

    fixed scenario 4 added curvature set in scenarios

--- test_scenario1.py
+++ test_scenario1.py
@@ -13,6 +13,7 @@ start, start_yaw, goal, goal_yaw = Point(1, 1), 0, Point (19, 19), 0
 simple_Scenario = Scenario_class.Scenario(env_width=20, env_height=20, boundary_collision=True)
 simple_Scenario.set_obstacles(obstacles)
 simple_Scenario.set_start_goal(start, start_yaw, goal, goal_yaw)
+simple_Scenario.set_vehicle(0.6)
 
 RRT(1000, simple_Scenario)
 simple_Scenario.plot_scenario()
