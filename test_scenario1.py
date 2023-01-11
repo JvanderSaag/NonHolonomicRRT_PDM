27e410c97d003f17ac7c0b738647ddbe28bb1a2f
HugoChass <62014029+HugoChass@users.noreply.github.com>
2023-01-11 17:08:59 +0100

    added folder for saved scenario with example in test_scenario added example function to save csv and read

--- test_scenario1.py
+++ test_scenario1.py
@@ -11,10 +11,13 @@ ObstacleCreator.create_rectangle(10, 10, (5, 5))
 obstacles = ObstacleCreator.return_obstacles()
 start, start_yaw, goal, goal_yaw = Point(1, 1), 0, Point (19, 19), 0
 
-simple_Scenario = Scenario(env_width=20, env_height=20, boundary_collision=True)
+simple_Scenario = Scenario('Scenario1', env_width=20, env_height=20, boundary_collision=True)
 simple_Scenario.set_obstacles(obstacles)
 simple_Scenario.set_start_goal(start, start_yaw, goal, goal_yaw)
 simple_Scenario.set_vehicle(0.6)
 
-RRT(1000, simple_Scenario)
+#RRT(1000, simple_Scenario, star=False, backwards=True)
+
+#simple_Scenario.write_csv('test1')
+simple_Scenario.read_csv('test1', set_path=True)
 simple_Scenario.plot_scenario()
