63d5d088ba628046791d5df454c94452efc0b07d
Ayush Kulshreshtha <A.Kulshreshtha-1student.tudelft.nl>
2023-01-15 16:55:39 +0100

    Tweaking controller further

---
+++ csv_utils.py
@@ -0,0 +1,8 @@
+csv_keys={
+    "ParallelParking_Scenario_ReedShepp":"ReedsSchepp_43.53_2k",
+    "ParallelParking_Scenario_Dubin":"Dubin_04.26_5k",
+    "Parking_Scenario_ReedsShepp":"ReedsShepp_22.59_2k",
+    "Parking_Scenario_Dubin":"Dubins_5.25_20k",
+    "Street_Scenario_ReedsShepp":"ReedsShepp_12.56_2k",
+    "Street_Scenario_Dubin":"Dubins_39.54_20k"
+}
