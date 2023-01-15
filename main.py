74a809fa5df32c6f910d2557225bdc82f5fe0c1a
Ayush Kulshreshtha <A.Kulshreshtha-1student.tudelft.nl>
2023-01-15 20:12:40 +0100

    All videos and plots added

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ ParallelParking_Scenario_Dubin
 Parking_Scenario_ReedsShepp
 Parking_Scenario_Dubin
 Street_Scenario_ReedsShepp
-Street_Scenario_Dubin
+Street_Scenario_Dubinrr
 '''
 
 from bin.run_controller import run_sim
-from Street_Scenario_Dubin import TestScenario
+from Parking_Scenario_Dubin import TestScenario
 
-run_sim(TestScenario, "Street_Scenario_Dubin", animate=True)
+run_sim(TestScenario, "Parking_Scenario_Dubin", animate=True)
 
